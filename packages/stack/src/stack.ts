0951f51cd1dd1a57bde11f59ba0ac25e462f3dfa
Wyatt Barnes <wyattlbarnes@gmail.com>
2020-04-17 15:22:10 -0700

    Moved to a workspaces format

---
+++ packages/stack/src/stack.ts
@@ -0,0 +1,141 @@
+/**
+ * A node used to hold stack data. It contains a one-way link to the next item
+ * down the stack.
+ */
+export type StackNode<TValue> = {
+    /**
+     * The next item down the stack
+     */
+    next?: StackNode<TValue>,
+    /**
+     * The data stored within the node
+     */
+    value: TValue,
+};
+
+/**
+ * A data structure that is first-in-last-out (FILO).
+ */
+export class Stack<TValue> {
+    #head?: StackNode<TValue>;
+    #size = 0;
+
+    /**
+     * Creates a stack. Items passed in will be pushed onto the stack.
+     * 
+     * @param items The items to push onto the stack
+     */
+    static create<TValue>(...items: TValue[]): Stack<TValue> {
+        return new Stack(...items);
+    }
+
+    /**
+     * Creates a stack. Items passed in will be pushed onto the stack.
+     * 
+     * @param items The items to push onto the stack
+     */
+    constructor(...items: TValue[]) {
+        items.forEach(item => {
+            this.push(item);
+        });
+    }
+
+    /**
+     * The number of items pushed onto the stack.
+     */
+    get size(): number {
+        return this.#size;
+    }
+
+    /**
+     * Pushes a value on to the top of the stack.
+     * 
+     * @param value The value to push on to the top of the stack
+     * @returns This stack
+     */
+    push(value: TValue): this {
+        // If head exists, we have items in the stack, so update the references
+        // as to "push" the item onto the stack. Otherwise, set create a node
+        // and set head to it.
+        if (this.#head) {
+            this.#head = {
+                next: this.#head,
+                value,
+            };
+        } else {
+            this.#head = {
+                value,
+            };
+        }
+
+        // Regardless of the outcome, stack will always increase, so update
+        // size
+        this.#size += 1;
+
+        return this;
+    }
+
+    /**
+     * Pops a value off the top of the stack, and then removes it.
+     * 
+     * @returns The value off the top of the stack
+     */
+    pop(): TValue | null {
+        // If head is not defined, just return null
+        if (!this.#head) {
+            return null;
+        }
+
+        // Get the value off the top of the stack
+        const value = this.#head.value;
+
+        // "Remove" the value from the stack (simply unset it)
+        this.#head = this.#head.next;
+
+        // Decrease the size of the stack
+        this.#size -= 1;
+
+        return value;
+    }
+
+    /**
+     * Gets the value off the top of the stack, but doesn't remove it.
+     * 
+     * @returns The value off the top of the stack
+     */
+    peek(): TValue | null {
+        // If head is not defined, just return null
+        if (!this.#head) {
+            return null;
+        }
+
+        return this.#head.value;
+    }
+
+    /**
+     * Returns an array representation of the stack.
+     * 
+     * @returns An array representation of the stack
+     */
+    toArray(): TValue[] {
+        const array: TValue[] = [];
+
+        let cursor = this.#head;
+
+        while (cursor) {
+            array.push(cursor.value);
+            cursor = cursor.next;
+        }
+
+        return array;
+    }
+
+    /**
+     * Returns a string representation of the stack.
+     * 
+     * @returns A string representation of the stack
+     */
+    toString(): string {
+        return this.toArray().toString();
+    }
+}
